c505396278942891229040295ee6cd024a0b1a40
wangyuan0810@163.com <wangyuan0810@163.com>
2020-10-17 09:08:20 +0800

    1

--- src/pages/analy/HistorySummarySimplePage.vue
+++ src/pages/analy/HistorySummarySimplePage.vue
@@ -17,6 +17,20 @@
       </FormItem>
       <Table border :columns="tableColumns" :data="tableRows"></Table>
     </Form>
+    <br>
+    <Row style="text-align: center">
+        <Page
+          ref="pageView"
+          :page-size-opts="this.pageModel.pageNums"
+          :total="this.pageModel.totalCount"
+          :current="this.pageModel.currentPage"
+          :page-size="this.pageModel.pageCount"
+          placement="top"
+          @on-change="onPageChange()"
+          @on-page-size-change="onPageChange()"
+          show-sizer
+        />
+    </Row>
   </div>
 </template>
 
@@ -37,6 +51,13 @@
 
     data() {
       return {
+        pageModel: {
+          totalCount: 100,
+          pageCount: 20,
+          currentPage: 1,
+          pageNums: [20, 30, 50, 80, 100]
+        },
+
         formModel: {
           type: 'playerId',
           roleId: ''
@@ -75,16 +96,20 @@
     },
 
     methods: {
+      onPageChange() {
+        this.pageModel.currentPage = this.$refs.pageView.currentPage;
+        this.pageModel.pageCount = this.$refs.pageView.currentPageSize;
+        this.handleSubmit();
+      },
 
       handleSubmit() {
-
         services.getHttpClient().post({
             url: "/dragon/statisticsSimpleList",
             body: {
             username: services.getUser().username,
             platform: services.getUser().platform,
-            page:1,
-            pageSize:500
+             page:this.pageModel.currentPage,
+            pageSize:this.pageModel.pageCount
           }
         }, (error, response, body) => {
           if (error) {
